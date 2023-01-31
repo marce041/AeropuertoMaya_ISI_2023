56169ea986d86df015f6d7bc87b1aaa1b4af510b
Marcela Sauceda <amarcesauceda@gmail.com>
2023-01-31 16:37:14 -0600

    Update aeropuerto.php

--- DatosMaestros/aeropuerto.php
+++ DatosMaestros/aeropuerto.php
@@ -160,7 +160,7 @@
                         <div class="card-body">
                             <form class="row g-3 needs-validation" action="../Procesos/Guardar/aeropuertoAdd.php" method="POST" enctype="multipart/form-data">
                                 <div class="mb-3">
-                                  <input class="form-control" name="nombre" type="text" placeholder="Nombre" onkeypress="return event.charCode>=65 && event.charCode<=90 || event.charCode>=97 && event.charCode<=122 || event.keyCode" minlength="4" maxlength="40" pattern="^[a-zA-Z|ñÑ ]+$" required>
+                                  <input class="form-control" name="nombre" id = "nombre" type="text" placeholder="Nombre" onkeypress="return event.charCode>=65 && event.charCode<=90 || event.charCode>=97 && event.charCode<=122 || event.keyCode" minlength="4" maxlength="40" pattern="^[a-zA-Z|ñÑ ]+$" required>
                                 </div>
                                 <div class="mb-3">
                                   <input class="form-control" name="hangar" type="text" placeholder="Hangar" onkeypress="return event.charCode>=48 && event.charCode<=57 || event.charCode>=65 && event.charCode<=90 || event.keyCode"  minlength="2" maxlength="8" required>
@@ -234,7 +234,7 @@
         crossorigin="anonymous"></script>
 </body>
 <script type="text/javascript">
-    
+    function validar letras{
     const expresiones = {       
         nombre: "/^[a-zA-ZÀ-ÿ\s]{4,15}$/", // Letras y espacios, pueden llevar acentos.       
     } 
@@ -250,7 +250,7 @@
             estado.innerHTML = "Incorrecto";
       //aqui podrias decirle al usuario que el valor es incorrecto
         }
-    });
+    });}
 </script>
 
 </html>
