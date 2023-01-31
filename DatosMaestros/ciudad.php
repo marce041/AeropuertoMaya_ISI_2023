80eb25a7a23f33bf7a6f9232955d58308dea1746
Marcela Sauceda <amarcesauceda@gmail.com>
2023-01-31 17:13:44 -0600

    Update ciudad.php

--- DatosMaestros/ciudad.php
+++ DatosMaestros/ciudad.php
@@ -170,7 +170,7 @@
                                 </div>
                                
                                 <div class="mb-3">
-                                    <label for="rol" text-align:left>País</label>
+                                    <label for="rol" class="text-right">País</label>
                                     <select class="form-select" name="estado" id="pais">
                                     
                                     <?php 
