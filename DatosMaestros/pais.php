6ea62a77ba93e7ade1595190079ea6e63ed24601
Marcela Sauceda <amarcesauceda@gmail.com>
2023-01-31 17:40:04 -0600

    Update pais.php

--- DatosMaestros/pais.php
+++ DatosMaestros/pais.php
@@ -142,13 +142,13 @@
                     <!-- Tajeta y Contenido -->
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">Insertar Datos
+                            <h6 class="m-0 font-weight-bold text-primary">Insertar datos
                             </h6>
                         </div>
                         <div class="card-body">
                             <form class="row g-3 needs-validation" action="../Procesos/Guardar/paisAdd.php" method="POST" enctype="multipart/form-data">
                                 <div class="mb-3">
-                                  <input class="form-control" name="nombre" type="text" placeholder="Nombre" onkeypress="return event.charCode>=65 && event.charCode<=90 || event.charCode>=97 && event.charCode<=122"  minlength="4" maxlength="20" required>
+                                <input class="form-control" name="nombre" id = "nombre" type="text" placeholder="Nombre" onkeypress="return event.charCode>=65 && event.charCode<=90 || event.charCode>=97 && event.charCode<=122 || event.keyCode" minlength="4" maxlength="50" pattern="^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$" required>
                                 </div>
                                 <div class="mb-3">
                                     <label for="rol">Región</label>
