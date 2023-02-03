d3be5944fb0450fcbd3c8063dd1fa57326306415
JosueSauceda <86754212+EstefaValle@users.noreply.github.com>
2023-02-02 19:58:26 -0600

    Error 36

--- DatosMaestros/moneda.php
+++ DatosMaestros/moneda.php
@@ -72,7 +72,7 @@
                         <a class="collapse-item" href="conversion.php">Conversion</a>
                         <a class="collapse-item" href="checkin.php">Check-In</a>
                         <a class="collapse-item" href="equipaje.php">Equipaje</a>
-                        <a class="collapse-item" href="detalle.php">Detalles</a>
+                        <a class="collapse-item" href="detallefactura.php">Detalles</a>
                         <a class="collapse-item" href="parametros.php">Parámetros</a>
                     </div>
                 </div>
