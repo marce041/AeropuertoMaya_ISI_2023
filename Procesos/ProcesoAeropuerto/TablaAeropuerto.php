e364c3b258367b9e7f1855ade7b86510fd9c2891
Marcela Sauceda <amarcesauceda@gmail.com>
2023-02-06 16:38:01 -0600

    Update TablaAeropuerto.php

--- Procesos/ProcesoAeropuerto/TablaAeropuerto.php
+++ Procesos/ProcesoAeropuerto/TablaAeropuerto.php
@@ -13,7 +13,7 @@
 
             echo 
             "
-                <table class='table table-lg table-dark table-responsive-lg table-bordered'>
+                <table class='table table-lg table-dark table-responsive-lg table-bordered' style='width:100%'>
                     <thead>
                         <tr class='text-center'>
                             <th scope='col'>Id del aeropuerto</th>
