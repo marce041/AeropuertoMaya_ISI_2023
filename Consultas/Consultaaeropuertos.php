71839c8911c24598c129d77801018c9fffc0488d
Marcela Sauceda <amarcesauceda@gmail.com>
2023-02-06 16:49:37 -0600

    Update Consultaaeropuertos.php

--- Consultas/Consultaaeropuertos.php
+++ Consultas/Consultaaeropuertos.php
@@ -146,7 +146,7 @@
 
                 <div class="card-body">
                     <div class="card shadow mb-4">
-                        <div class="card-header py-3">
+                        <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                             <h6 class="m-0 font-weight-bold text-primary">Lista de aeropuertos</h6>
                         </div>
                         <div class="card-body">
