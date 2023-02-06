aa05f42d7b914635db1a81a59b2808a91dcd2ea8
Marcela Sauceda <amarcesauceda@gmail.com>
2023-02-06 16:48:09 -0600

    Update Consultaaeropuertos.php

--- Consultas/Consultaaeropuertos.php
+++ Consultas/Consultaaeropuertos.php
@@ -19,7 +19,7 @@
         require("../Procesos/ProcesoAeropuerto/TablaAeropuerto.php"); 
     ?>
 <body id="page-top">
-    <div id="wrapper" class="card-body">
+    <div id="wrapper">
         <!--Empieza Lista -->
         <ul class="navbar-nav color sidebar sidebar-dark accordion" id="accordionSidebar">
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="../principaladmin.php">
@@ -144,10 +144,10 @@
                     </ul>
                 </nav>
 
-                <div class="container-fluid">
+                <div class="card-body">
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">Tabla Aeropuertos</h6>
+                            <h6 class="m-0 font-weight-bold text-primary">Lista de aeropuertos</h6>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
