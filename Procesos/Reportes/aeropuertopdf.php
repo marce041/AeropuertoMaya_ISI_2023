4f2b16904ab732787a17e4a3e525bfed76d4ba61
Estefany Valle <86754212+EstefaValle@users.noreply.github.com>
2023-02-24 06:43:39 -0600

    aeropuerto PDF Y XLSX

--- Procesos/Reportes/aeropuertopdf.php
+++ Procesos/Reportes/aeropuertopdf.php
@@ -1,12 +1,31 @@
 <?php
+session_start();
 require('fpdf.php');
+require "../../conexion.php";
+
+
+$user=$_SESSION['idUser'];
+$queryparametro=mysqli_query($conn, "SELECT Usuario FROM usuario WHERE `idUser`=$user;");
+    
+    $rangini = array();
+  
+    while($datos = mysqli_fetch_array($queryparametro)) {
+        array_push($rangini, $datos['Usuario']);
+    }
+
+    $rangoinicial=$rangini[0];
+
+    date_default_timezone_set('America/Mexico_City');
+
+    $fechaActual = date("d-m-Y");
+    $horaActual = date("H:i:s");
 
 class PDF extends FPDF
 {
 // Cabecera de página
 function Header()
 {
-    // Logo
+ // Logo
      $this->Image('img/Emirates-Logo-1985-19991.png',10,8,40);
     // Arial bold 15
     $this->SetFont('Arial','B',18);
@@ -15,13 +34,35 @@ function Header()
     // Movernos a la derecha
     $this->Cell(60);
     // Título
-    $this->Cell(70,10,'Reporte de Aeropuertos',0,0,'C');
+    $this->Cell(70,10,'Reporte de Aeronaves',0,0,'C');
+    
+    $this->SetFont('Arial','',12);
+	$this->SetTextColor(39,39,51);
+	$this->Cell(150,9,utf8_decode(""),0,0,'L');
+
+	$this->Ln(10);
+    $this->Cell(150);
+	
+	$this->Cell(250,9,utf8_decode("Usuario: ".$GLOBALS["rangoinicial"]),0,0,'L');
+
+	$this->Ln(7);
+    $this->Cell(150);
+	$this->Cell(150,9,utf8_decode("Fecha: ".$GLOBALS["fechaActual"] ),0,0,'L');
+
+	$this->Ln(7);
+    $this->Cell(150);
+	$this->Cell(150,9,utf8_decode("Hora: ".$GLOBALS["horaActual"]),0,0,'L');
+	
+    $this->Ln(7);
+    $this->Cell(117,7,utf8_decode(''),'',0,'C');
+    $this->Cell(10,7,utf8_decode('_____________________________________________________________________________________________________________________________'),'',0,'C');
     // Salto de línea
+    $this->SetFont('Arial','B',18);
     $this->Ln(25);
     $this->Cell(30);
     $this->cell(50,10,'Nombre',1,0,'C',0);
     $this->cell(42,10,'Hangar',1,0,'C',0);
-    $this->cell(40,10,'Id_Ciudad',1,0,'C',0);
+    $this->cell(40,10,'Ciudad',1,0,'C',0);
     $this->Ln(10);
     $this->Cell(25);
 }
@@ -29,12 +70,18 @@ function Header()
 // Pie de página
 function Footer()
 {
-    // Posición: a 1,5 cm del final
-    $this->SetY(-15);
-    // Arial italic 8
-    $this->SetFont('Arial','I',8);
-    // Número de página
-    $this->Cell(0,10,utf8_decode('Pagina').$this->PageNo().'/{nb}',0,0,'C');
+   // Posición: a 1,5 cm del final
+   $this->SetFont('Arial','',12);
+   $this->SetTextColor(39,39,51);
+   $this->Ln(7);
+   $this->Cell(117,7,utf8_decode(''),'',0,'C');
+   $this->Cell(10,7,utf8_decode('_____________________________________________________________________________________________________________________________'),'',0,'C');
+   
+   $this->SetY(-15);
+   // Arial italic 8
+   $this->SetFont('Arial','I',8);
+   // Número de página
+   $this->Cell(0,10,utf8_decode('Página ').$this->PageNo().'/{nb}',0,0,'C');
 }
 }
 
--- Procesos/Reportes/aeropuertoxlsx.php
+++ Procesos/Reportes/aeropuertoxlsx.php
@@ -1,16 +1,46 @@
 <?php
 header('Content-type:application/xls');
 header('Content-Disposition: attachment; filename=Aeropuerto.xls');
+require "../../conexion.php";
+session_start();
+
+
+
+$user=$_SESSION['idUser'];
+$queryparametro=mysqli_query($conn, "SELECT Usuario FROM usuario WHERE `idUser`=$user;");
+    
+    $rangini = array();
+  
+    while($datos = mysqli_fetch_array($queryparametro)) {
+        array_push($rangini, $datos['Usuario']);
+    }
+
+    $rangoinicial=$rangini[0];
+
+    date_default_timezone_set('America/Mexico_City');
+
+    $fechaActual = date("d-m-Y");
+    $horaActual = date("H:i:s");
 ?>
 
 <table border="1">
 <tr>
-    <th colspan=3>Reporte de Aeropuertos</th>
+    <th colspan=12>Reporte de Aeropuerto</th>
+<?php
+echo "
+
+    <th>Usuario: $rangoinicial</th>
+    <th>Fecha: $fechaActual</th>
+    <th>Hora: $horaActual</th>
+  
+    ";
+    ?>
     </tr>
     <tr>
-    <th>Nombre</th>
-    <th>Hangar</th>
-    <th>Id_Ciudad</th>
+    <th colspan=3>Nombre</th>
+    <th colspan=3>Hangar</th>
+    <th colspan=3>Id_Ciudad</th>
+    <th colspan=3></th>
 </tr>
 <?php require "../../conexion.php";
 $consulta="SELECT * from aeropuerto";
@@ -18,10 +48,10 @@ $resultado=$conn->query($consulta);
 
 while($row=$resultado->fetch_assoc()){
    echo "<tr>
-    <td>$row[Nombre]</td>
-    <td>$row[Hangar]</td>
-    <td>$row[Id_Ciudad]</td>
-    
+    <td colspan=3>$row[Nombre]</td>
+    <td colspan=3>$row[Hangar]</td>
+    <td colspan=3>$row[Id_Ciudad]</td>
+    <td colspan=3></td>
     </tr>
     ";
 }
