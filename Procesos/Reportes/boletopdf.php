49edc6cfc7dbe118a8af74db26b635551b51a095
Marcela Sauceda <amarcesauceda@gmail.com>
2023-03-08 23:06:24 -0600

    Update boletopdf.php

--- Procesos/Reportes/boletopdf.php
+++ Procesos/Reportes/boletopdf.php
@@ -60,7 +60,7 @@ function Header()
     $this->Cell(117,7,utf8_decode(''),'',0,'C');
     $this->Cell(10,7,utf8_decode('_____________________________________________________________________________________________________________________________'),'',0,'C');
     // Salto de línea
-/*
+
     $this->SetFont('Arial','B',12);
      // Color de texto
 
@@ -92,7 +92,7 @@ function Footer()
     // Número de página
     $this->Cell(0,10,utf8_decode('Página ').$this->PageNo().'/{nb}',0,0,'C');
 }
-
+}
 require "../../conexion.php";
 $consulta="SELECT * from boleto";
 $resultado=$conn->query($consulta);
@@ -117,34 +117,8 @@ while($row=$resultado->fetch_assoc()){
 
     $pdf->Ln(10);
   
-
    
-   
-    $pdf->SetFont('Arial','',14);
-
-    while($row=$resultado->fetch_assoc()){
-        $pdf->Cell(5);
-        $pdf->cell(50,10,$row['Codigo'],1,0,'C',0);
-        $pdf->cell(42,10,$row['Id_Asiento'],1,0,'C',0);
-    
-        $pasajero=$row['Id_Pasajero'];
-    $querypasajero=mysqli_query($conn, "SELECT Nombre FROM pasajero WHERE `Id_Pasajero`=$pasajero;");
-        
-        $rang = array();
-      
-        while($datos = mysqli_fetch_array($querypasajero)) {
-            array_push($rang, $datos['Nombre']);
-        }
-    
-        $rangpaso=$rang[0];
-        $pdf->cell(40,10,$rangpaso,1,0,'C',0);
-        $pdf->cell(50,10,$row['Precio'],1,0,'C',0);
-    
-        $pdf->Ln(10);
-       
-
-    
-    }
+}
     $pdf->Output();
 
 ?>    
