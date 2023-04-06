d312d1080b861404bd4803a3e5b796a918eb3649
Marcela <amarcesauceda@gmail.com>
2023-04-06 14:05:43 -0600

    diseno login

--- index.php
+++ index.php
@@ -67,9 +67,9 @@ INNER JOIN pantallas p ON p.Id_Pantalla = rp.Id_Pantalla
  <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" 
  crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
-<body>
+<body style="background-image: url('http://localhost/AeropuertoMaya_ISI_2023/img/jeje.jpg');">
 	<main>
-		<form action="<?php $_SERVER["PHP_SELF"]; ?>" method="POST" class="formulario" id="formulario" >
+		<form action="<?php $_SERVER["PHP_SELF"]; ?>" method="POST" class="formulario" id="formulario" style="background-color: rgba(58, 56, 56, 0.4);">
 			<table>
 				
 			<tr>
